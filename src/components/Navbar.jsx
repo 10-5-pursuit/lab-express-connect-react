20a0231f68da389fe24408915d8890d47d044c79
Diamond <dbrown@pursuit.org>
2024-06-29 23:57:59 -0400

    created and styled navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+     <div className='page-title'>Captain's Log</div>
+     <input className='submit' type="submit" value="NEW LOG"/>
+    </div>
+  )
+}
+
+export default Navbar
